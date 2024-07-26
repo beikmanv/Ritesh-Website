efd53d731b54ce0c419f69d0b13d6ebef568f8d0
Valdis <v.beikmanis@gmail.com>
2024-07-26 12:58:16 +0100

    Serve media files in development

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,6 +127,11 @@ USE_TZ = True
 
 import os
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Add this line
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -2,11 +2,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', lambda request: redirect('gallery/', permanent=True)),
     path('', include('myapp.urls')),  # This includes the URLs from myapp
-]
-
-
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

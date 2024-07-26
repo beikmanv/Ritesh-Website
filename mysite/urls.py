2437fddb6eee5451fcb95983a5d2811627bcc1db
Valdis <v.beikmanis@gmail.com>
2024-07-26 12:49:09 +0100

    Add root URL redirection to gallery

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,10 +1,13 @@
 # mysite/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),
+    path('', lambda request: redirect('gallery/', permanent=True)),
+    path('upload/', include('myapp.urls')),
+    path('gallery/', include('myapp.urls')),
 ]
 
 
